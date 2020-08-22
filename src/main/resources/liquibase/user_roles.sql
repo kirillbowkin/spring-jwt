8b2f9879e71868b248d8299fe2cb2f6b6bb53981
Kirill Shishkin <kirillshishkin77@gmail.com>
2020-08-22 05:07:44 +0300

    Initial commit of a project

---
+++ src/main/resources/liquibase/user_roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_roles (user_id, role_id) VALUES ('6d3ddc48-3b95-474a-9595-55319b9ff85d', 'd508b26e-6cc8-47a5-be1d-5e611cc4c03f');
+INSERT INTO user_roles (user_id, role_id) VALUES ('6d3ddc48-3b95-474a-9595-55319b9ff85d', 'd920dcda-561a-40aa-9207-04328e40052b');
+INSERT INTO user_roles (user_id, role_id) VALUES ('33cbbda7-364a-4c0c-90ba-c788e18a56ce', 'd508b26e-6cc8-47a5-be1d-5e611cc4c03f');
---
+++ src/main/resources/liquibase/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, username, password, first_name, last_name, email)
+VALUES ('6d3ddc48-3b95-474a-9595-55319b9ff85d','kirill', '$2y$12$hb90rjYZJskbNYVELMaNV.crTYnYSMpqi6HUOTspuupy1.FSX3UPi', 'kirill', 'shishkin', 'kirill@gmail.com');
+
+INSERT INTO users (id, username, password, first_name, last_name, email)
+VALUES ('33cbbda7-364a-4c0c-90ba-c788e18a56ce', 'userone', '$2y$12$DzHQOsNDulqqQ3/3TirHWOGap8VwWLKGtZqmTshdlUCR6490e5Xum', 'test', 'test', 'userone@gmail.com');
